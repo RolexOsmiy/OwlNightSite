364248568f65ee119669d51b5e6a22a3ed451352
ValentinSegeda <walik-segeda@mail.ru>
2016-08-13 19:22:41 +0300

    Global Update

--- OwlNightSite/js/javascript.js
+++ OwlNightSite/js/javascript.js
@@ -1,15 +1,18 @@
-function valid (form) {
+ $( document ).ready(function() {
+      $('#submit-button').on('click', function() {
+            var form = $('#form');
             var fail = false;
-            var textUser = form.textUser.value;
-            var email = form.email.value;
-            var job = form.job.value;
-            
+            var textUser = form.context.mailJob.textUser.value;
+            var email = form.context.mailJob.email.value;
+            var job = form.context.mailJob.job.value;
+
             var adr_pattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,3}/i;
-            
-            else if (adr_pattern.test(email) == false) fail = "Некорректный E-mail!";
+
+            if (adr_pattern.test(email) == false) fail = "Некорректный E-mail!";
             else if (job == "") fail = "Укажите должность!";
             if (textUser == "" || textUser == " ") fail = "Вы не ввели свое имя!";
-            
+
             if(fail) alert(fail);
             else window.location = "jobs.html";
-        }
+      });
+});
